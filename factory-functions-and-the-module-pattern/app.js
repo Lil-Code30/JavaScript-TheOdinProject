9e8092785d66de110494f345222485c92600f857
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-03-07 05:05:27 -0500

    starting to learn closure in JS

---
+++ factory-functions-and-the-module-pattern/app.js
@@ -0,0 +1,59 @@
+let globalAge = 23; // This is a global variable
+
+// This is a function - and hey, a curly brace indicating a block
+function printAge (age) {
+  var varAge = 34; // This is a function scoped variable
+
+  // This is yet another curly brace, and thus a block
+  if (age > 0) {
+    // This is a block-scoped variable that exists
+    // within its nearest enclosing block, the if's block
+    const constAge = age * 2;
+    console.log(constAge);
+  }
+
+  // ERROR! We tried to access a block scoped variable
+  // not within its scope
+  // console.log(constAge);
+}
+
+printAge(globalAge);
+
+// ERROR! We tried to access a function scoped variable
+// outside the function it's defined in
+// console.log(varAge);
+
+
+// Closures aren’t scary
+// A closure is a function that has access to its own scope, the scope of its parent function
+// and the global scope.
+
+function makeAdding(firstNumber){
+    //"first" is scoped within the makeAdding function
+    const first = firstNumber;
+    return function resulting(secondNumber){
+        // "second" is scoped within the resulting function
+        const second = secondNumber;
+        return first + second;
+    }
+}
+
+// We can use the closure like this
+const addFive = makeAdding(5);
+console.log(addFive(10)); // Outputs: 15
+
+
+// Factory functions 🏭
+// A factory function is a function that returns a new object.
+// It's like a constructor function, but without the "new" keyword.
+
+const User = function(name){
+    this.name = name;
+    this.discordName = "@" + name;
+}
+// this is a constructor - then this can be refactored into a fctory
+
+function createUser(name){
+    const discordName = "@" + name;
+    return {name, discordName};
+}
