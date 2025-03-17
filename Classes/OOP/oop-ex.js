f59402dbbe1afe5872886ee68d9613fe129b9616
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-03-17 13:48:28 -0400

    Ex4 BankAccount Class with Deposit and Withdrawal

--- Classes/OOP/oop-ex.js
+++ Classes/OOP/oop-ex.js
@@ -87,3 +87,34 @@ const myCar = new Car('Toyota', 'Camry', 2015, 4);
 myCar.displayDetails();
 
 console.log("_____ End of Ex3 _____");
+
+// Ex4 BankAccount Class with Deposit and Withdrawal
+/*
+Write a JavaScript program that creates a class called "BankAccount" with properties for account number and balance. Include methods to deposit and withdraw money from the account. Create some instances of the "BankAccount" class, deposit some money, and withdraw a portion of it.
+*/ 
+
+class BankAccount{
+    constructor(accountNumber, balance){
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    deposit(amount){
+        this.balance += amount;
+        console.log(`Deposited ${amount} into account ${this.accountNumber}. New balance: ${this.balance}`);
+    }
+
+    withdraw(amount){
+        if(amount > this.balance){
+            console.log(`Insufficient funds in account ${this.accountNumber}. Withdrawal of ${amount} is not possible.`);
+        }else{
+            this.balance -= amount;
+            console.log(`Withdrew ${amount} from account ${this.accountNumber}. New balance: ${this.balance}`);
+        }
+    }
+}
+
+const account1 = new BankAccount(123456, 1000);
+account1.deposit(500);
+account1.withdraw(200);
+account1.withdraw(1500);
