d05b26c5479037f083dd90ee276be8a92592ddd3
zyphorah <zyphorah.zen@gmail.com>
2025-03-25 20:50:27 -0400

    retirer les verifications d'erreur ainsi que async

--- JSON/script.js
+++ JSON/script.js
@@ -2,25 +2,20 @@
 //Elle return un objet utilisable, data.datanom
 function objetJson() 
 {
-    return fetch('superheroes.json')
-      .then(response => {
-        if (!response.ok) throw new Error('Erreur HTTP ' + response.status);
-        return response.json();
-      })
-      .catch(err => {
-        console.error("Erreur lors de la lecture du fichier JSON :", err);
-        throw err;
-      });
+    return fetch('superheroes.json').then(response => response.json());
 }
 
-async function afficherObjet() 
+function afficherObjet() 
 {
       // Ici, nous pouvons utiliser l'objet retourné par la fonction objetJson.
     // Vous pouvez ensuite manipuler cet objet comme vous le souhaitez.
     // N'oubliez pas d'utiliser cette fonction dans une fonction async avec await,
     // afin d'attendre la récupération complète de l'objet JSON.
-      const data = await objetJson();
-      console.log(data);
+    //Il faut maintenant utiliser .then car les alwai ne sont plus là
+    objetJson().then(data => {
+        //Et donc tu fait le traitement ici 
+        console.log(data.squadName);
+    });
   
 }
   
