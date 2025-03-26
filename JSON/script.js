cfa5f16976f4c90c51c5de893c7cf6fec0397b22
zyphorah <zyphorah.zen@gmail.com>
2025-03-25 20:14:24 -0400

    correction problème

--- JSON/script.js
+++ JSON/script.js
@@ -1,2 +1,10 @@
-let data = JSON.parse(superheroes);
-console.log(data);
+fetch('superheroes.json')
+  .then(response => {
+    if (!response.ok) throw new Error('Erreur HTTP ' + response.status);
+    return response.json();
+  })
+  //C'est ici que nous obtenons l'objet de manipulation
+  //Tu peux le manipuler avec data.nomdedata
+  
+  .then(data => console.log(data.squadName))
+  .catch(err => console.error("Erreur lors de la lecture du fichier JSON :", err));
