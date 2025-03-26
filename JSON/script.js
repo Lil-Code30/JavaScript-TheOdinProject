b8f97c654cb666e53c1ec1e66c454e831a2c9eaf
zyphorah <zyphorah.zen@gmail.com>
2025-03-25 20:38:50 -0400

    Inclusion de la lecture du fichier json dans une fonction

--- JSON/script.js
+++ JSON/script.js
@@ -1,10 +1,27 @@
-fetch('superheroes.json')
-  .then(response => {
-    if (!response.ok) throw new Error('Erreur HTTP ' + response.status);
-    return response.json();
-  })
-  //C'est ici que nous obtenons l'objet de manipulation
-  //Tu peux le manipuler avec data.nomdedata
+
+//Elle return un objet utilisable, data.datanom
+function objetJson() 
+{
+    return fetch('superheroes.json')
+      .then(response => {
+        if (!response.ok) throw new Error('Erreur HTTP ' + response.status);
+        return response.json();
+      })
+      .catch(err => {
+        console.error("Erreur lors de la lecture du fichier JSON :", err);
+        throw err;
+      });
+}
+
+async function afficherObjet() 
+{
+      // Ici, nous pouvons utiliser l'objet retourné par la fonction objetJson.
+    // Vous pouvez ensuite manipuler cet objet comme vous le souhaitez.
+    // N'oubliez pas d'utiliser cette fonction dans une fonction async avec await,
+    // afin d'attendre la récupération complète de l'objet JSON.
+      const data = await objetJson();
+      console.log(data);
   
-  .then(data => console.log(data.squadName))
-  .catch(err => console.error("Erreur lors de la lecture du fichier JSON :", err));
+}
+  
+afficherObjet();
