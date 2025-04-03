7b85df07f7aca62ff34c8891ea06b3b472c05268
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-04-03 18:21:43 -0400

    final Code

--- Build-an-image-carousel/script.js
+++ Build-an-image-carousel/script.js
@@ -1,5 +1,34 @@
-function test() {
-  console.log("Test");
-}
+const nextBtn = document.querySelector("#next-button");
+const prevBtn = document.querySelector("#previous-button");
 
-const greet = "Hello";
+const images = document.querySelectorAll(".image-box");
+
+let currentImage = 0;
+
+const slidesImage = (n) => {
+  if (n < 0) {
+    currentImage = images.length - 1;
+  }
+
+  if (n > images.length - 1) {
+    currentImage = 0;
+  }
+
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+
+  images[currentImage].style.display = "block";
+};
+
+slidesImage(currentImage);
+
+nextBtn.addEventListener("click", () => {
+  currentImage++;
+  slidesImage(currentImage);
+});
+
+prevBtn.addEventListener("click", () => {
+  currentImage--;
+  slidesImage(currentImage);
+});
