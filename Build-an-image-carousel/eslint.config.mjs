0ae66661b2acc1b7197e986c6685fc76d2a6a755
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-04-03 16:30:18 -0400

    configuring the eslint and prettier in the projecet

---
+++ Build-an-image-carousel/eslint.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import eslintPluginPrettier from "eslint-plugin-prettier/flat";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended", eslintPluginPrettier],
+  },
+]);
