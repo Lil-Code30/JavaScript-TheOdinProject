7dcb74ea1bdffcb1cf099e4b9366a408c309ef96
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-04-22 04:14:51 -0400

    Collatz Conjecture with recursion metod

---
+++ recursion-methods/collatz-conjecture.js
@@ -0,0 +1,25 @@
+/*
+write a recursive function collatz(n) that calculates how many steps it takes to get to 1 if you start from n and recurse 
+
+conditions:
+    if n is 1, return 0
+    if n is even, return collatz(n/2)
+    if n is odd, return collatz(3*n + 1)
+*/
+
+let steps = 0;
+
+function collatz(n) {
+  if (n === 1) {
+    return steps;
+  } else if (n % 2 === 0) {
+    steps++;
+    return collatz(n / 2);
+  } else if (n % 2 !== 0) {
+    steps++;
+    return collatz(3 * n + 1);
+  }
+}
+
+collatz(27);
+console.log(`The number of steps is: ${steps}`);
